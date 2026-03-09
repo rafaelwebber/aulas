bdc5ee889efb9dc54597588eadf69cff51d07ad4
Rafael Lumertz Webber <rafael.webber@gmail.com>
2026-03-09 19:42:01 -0300

    firts commit

--- aula.py
+++ aula.py
@@ -1,1 +1,7 @@
-print("heloou")
+import numpy as np
+import skfuzzy as ctrl
+
+peso = ctrl.Antecedent(np.arange(20,120,1), 'peso')
+altura = ctrl.Antecedent(np.arange(1.2,2,0.1), 'altura')
+
+tamanho = ctrl.Antecedent(np.arange(0,1,0.1), 'tamanho')
